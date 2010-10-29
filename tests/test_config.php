8da04b3e3685ea8c3c0f46dbbb6e254e4b94080c
Jeremy Blanchard <auzigog@gmail.com>
2010-10-29 04:04:49 -0700

    Removing accidental addition of my config file

--- tests/test_config.php
+++ tests/test_config.php
@@ -4,12 +4,12 @@
  * Specify a Google Account username and password to use for testing.
  */
 
-$login_email = 'ggphpapi@gmail.com';      // Example: foobar@gmai.com
-$login_password = 'GoogleGroupsPHPAPI2010';   // Example: SeCrEt1337
+$login_email = '';      // Example: foobar@gmai.com
+$login_password = '';   // Example: SeCrEt1337
 
 /**
  * Specify a google group that the above account has manager or admin access to.
  * This group should not be safe to break incase a test does fail. This should
  * be the shortname of the group.
  */
-$test_group = 'ggphpapi-test';       // Example: my-test-group
+$test_group = '';       // Example: my-test-group
